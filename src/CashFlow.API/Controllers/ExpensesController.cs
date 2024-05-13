de28470fbffe8c1c303e045d979230b157d3353c
Uanderson Leonel <dev.uandz@gmail.com>
2024-05-13 07:02:09 -0300

    Add Register Route

---
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.API.Controllers;
+
+public class ExpensesController : CashFlowAPIBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredExpenseJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        try
+        {
+            var useCase = new RegisterExpenseUseCase();
+
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Unknown error");
+        }
+    }
+}
