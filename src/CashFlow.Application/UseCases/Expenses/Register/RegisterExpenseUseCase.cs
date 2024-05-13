8bb65ab2e3da5384e2ba49b3419c28bf4ccfeb74
Uanderson Leonel <dev.uandz@gmail.com>
2024-05-13 07:01:24 -0300

    Add Expense UseCase

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Title))
+        {
+            throw new ArgumentException("The title is required");
+        }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero");
+        }
+
+        if(DateTime.Compare(request.Date, DateTime.UtcNow) > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        if(!Enum.IsDefined(typeof(PaymentType), request.PaymentType)){
+            throw new ArgumentException("Payment type is not valid");
+        }
+    }
+}
